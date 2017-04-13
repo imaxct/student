a495fb1af82727e6d417ea3124208a427640b39c
imaxct <imaxct@foxmail.com>
2017-04-13 22:46:16 +0800

    format code.

--- src/main/java/imaxct/dao/impl/BaseDao.java
+++ src/main/java/imaxct/dao/impl/BaseDao.java
@@ -18,81 +18,83 @@ public class BaseDao<T> {
     @Resource(name = "sessionFactory")
     private SessionFactory sessionFactory;
 
-    protected Session currentSession(){
+    protected Session currentSession() {
         return sessionFactory.getCurrentSession();
     }
+
     private static Logger logger = LogManager.getLogger(BaseDao.class);
-    public boolean create(T t){
+
+    public boolean create(T t) {
         Session session = currentSession();
-        try{
+        try {
             session.persist(t);
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("create", e);
             session.getTransaction().rollback();
             return false;
         }
     }
 
-    public boolean delete(T t){
+    public boolean delete(T t) {
         Session session = currentSession();
-        try{
+        try {
             session.delete(t);
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             session.getTransaction().rollback();
             logger.error("delete", e);
             return false;
         }
     }
 
-    public boolean update(T t){
+    public boolean update(T t) {
         Session session = currentSession();
-        try{
+        try {
             session.update(t);
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             session.getTransaction().rollback();
             logger.error("update", e);
             return false;
         }
     }
 
-    public T find(Class<T> clazz, Serializable id){
+    public T find(Class<T> clazz, Serializable id) {
         return currentSession().get(clazz, id);
     }
 
-    public List<T> execute(String hql, Object... objects){
+    public List<T> execute(String hql, Object... objects) {
         Session session = currentSession();
-            Query query = session.createQuery(hql);
-            if (objects != null && objects.length > 0){
-                for (int i=0; i<objects.length; ++i){
-                    query.setParameter(i, objects[i]);
-                }
+        Query query = session.createQuery(hql);
+        if (objects != null && objects.length > 0) {
+            for (int i = 0; i < objects.length; ++i) {
+                query.setParameter(i, objects[i]);
             }
-            return query.list();
+        }
+        return query.list();
     }
 
-    public T uniqueResult(String hql, Object... objects){
+    public T uniqueResult(String hql, Object... objects) {
         Session session = currentSession();
-            Query query = session.createQuery(hql);
-            if (objects != null && objects.length > 0){
-                for (int i=0; i<objects.length; ++i){
-                    query.setParameter(i, objects[i]);
-                }
+        Query query = session.createQuery(hql);
+        if (objects != null && objects.length > 0) {
+            for (int i = 0; i < objects.length; ++i) {
+                query.setParameter(i, objects[i]);
             }
-            return (T) query.uniqueResult();
+        }
+        return (T) query.uniqueResult();
     }
 
-    public List<T> listPage(int firstPage, int maxResult, String hql, Object... objects){
+    public List<T> listPage(int firstPage, int maxResult, String hql, Object... objects) {
         Session session = currentSession();
-            Query query = session.createQuery(hql);
-            query.setFirstResult(firstPage).setMaxResults(maxResult);
-            if (objects != null && objects.length > 0){
-                for (int i=0; i<objects.length; ++i){
-                    query.setParameter(i, objects[i]);
-                }
+        Query query = session.createQuery(hql);
+        query.setFirstResult(firstPage).setMaxResults(maxResult);
+        if (objects != null && objects.length > 0) {
+            for (int i = 0; i < objects.length; ++i) {
+                query.setParameter(i, objects[i]);
             }
-            return query.list();
+        }
+        return query.list();
     }
 }
